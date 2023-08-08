b9ee6876cf9c676872bd41fd94eaa79856fcc4ad
Muhammad Bilal <2019cs682@student.uet.edu.pk>
2023-08-08 15:04:29 +0500

    started

---
+++ components/Tutorial.vue
@@ -0,0 +1,12 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="text-3xl text-blue-600">
+    Hello from Connectin
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
